3d02a025f9737e411769e1212baf06bdafb86401
Ankit Kumar Misra <ankitkumarmisra2001@gmail.com>
2023-03-14 16:24:26 +0530

    organized directory structure

---
+++ plain/main.py
@@ -0,0 +1,282 @@
+import os
+import heapq
+import random
+import shutil
+import argparse
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from utils.seed import rng, seeds
+np.random.seed(seeds["np.random.seed"])
+random.seed(seeds["random.seed"])
+
+from utils.definitions import Node, Block, Event, Transaction
+from utils import blkIdGen, txnIdGen, initLatency, eventq, get_blocks
+
+#The discrete event simulator
+class Simulation:
+    def __init__(self, n, ttx, z0, z1, I=600):
+        """
+        Initializes the simulation object
+            n: Number of Nodes
+            ttx: Mean inter-arrival time of transactions
+            z0: Percentage of slow nodes
+            z1: Percentage of low CPU nodes
+            I: Average block mining time
+        """
+        #building the connection graph of peers
+        self.G = nx.Graph()
+        self.G.add_nodes_from(range(n))
+
+        #genesis block
+        self.genesis = Block(pb=0, bid=1, txnIncluded=set(), miner=None, balance = [0]*n)
+
+        #id generation
+        self.blkid_generator = blkIdGen()
+        self.txnid_generator = txnIdGen()
+        
+        #speed and cpu as mentioned in the question
+        speed = ["slow" for i in range(int(n*z0))]+["fast" for i in range(n-int(n*z0))]
+        cpu = ["low" for i in range(int(n*z1))]+["high" for i in range(n-int(n*z1))]
+        rng.shuffle(speed)
+        rng.shuffle(cpu)
+
+        #hashing power
+        invh0 = n*(10 - 9*z1)
+        invh1 = invh0/10
+        miningTime = [I*invh0 if cpu[i] == "low" else I*invh1 for i in range(n)]
+        
+        self.nodes = [None]*n
+        for i in range(n):
+            self.nodes[i] = Node(nid=i, speed=speed[i], cpu=cpu[i],
+                                 genesis=self.genesis, miningTime=miningTime[i],
+                                 blkgen=self.blkid_generator,
+                                 txngen=self.txnid_generator)
+
+        self.ttx = ttx
+        self.I = I
+        initLatency(n)
+
+    def generate_network(self): #p2p network connection
+        n = len(self.nodes)
+        while not nx.is_connected(self.G):
+            self.G = nx.Graph()
+            self.G.add_nodes_from(range(n))
+            # reset nodes
+            for node in self.nodes:
+                node.peers = set()
+            # generate random connections
+            for nodeX in range(n):
+                l = rng.integers(4, 9)
+                #print(l)
+                while len(self.nodes[nodeX].peers) < l:
+                    nodeY = rng.choice([j for j in range(n) if j != nodeX and j not in self.nodes[nodeX].peers]) 
+                    if nodeY != nodeX:
+                        self.connection(nodeX, nodeY)
+                #print(len(self.nodes[nodeX].peers))
+            #print(self.G.edges)
+
+    #adding edges of the peer graph
+    def connection(self, nodeX, nodeY): #if x and y not connected then connect
+        if(nodeY not in self.nodes[nodeX].peers and nodeX not in self.nodes[nodeY].peers):
+            self.G.add_edge(nodeX, nodeY)
+            self.nodes[nodeX].peers.add(self.nodes[nodeY])
+            self.nodes[nodeY].peers.add(self.nodes[nodeX])
+
+    # initializing events and pushing to the queue - block and txn
+    def gen_all_txn(self, max_time): #generate event
+        for p in self.nodes:
+            minetime = rng.exponential(p.miningTime)
+            block2mine = Block( #first mined block
+                pb=self.genesis,
+                bid=next(self.blkid_generator),
+                txnIncluded=set([Transaction(
+                    peerX=-1,
+                    id=next(self.txnid_generator),
+                    peerY=p,
+                    value=50,
+                    case=2
+                )]),
+                miner=p
+            )
+            # adding block event to the queue
+            heapq.heappush(eventq, (minetime, Event(minetime, "BlockMined", block=block2mine)))
+            
+            # generate txn and add to the queue
+            t = rng.exponential(self.ttx)
+            while(t < max_time):
+                elem = Transaction(
+                    peerX=p,
+                    id = next(self.txnid_generator),
+                    peerY = self.nodes[rng.integers(0, len(self.nodes))],
+                    value = 0, case=1
+                )
+                heapq.heappush(eventq, (t, Event(t, "TxnGen", txn=elem)))
+                t = t + rng.exponential(self.ttx)
+
+    #running the simulation, running events from the queue and writing to file
+    def run(self, untill): #simulate untill
+        time = 0
+        while(time < untill and len(eventq) > 0):
+            time, event = heapq.heappop(eventq)
+            self.handle(event)
+        while(len(eventq) > 0):
+            time, event = heapq.heappop(eventq)
+            if event.event_type in ["TxnRecv", "BlockRecv"]:
+                self.handle(event)
+        
+        for i in self.nodes: #each node
+            file = open(f'./logs/log_tree_{i.nid}.txt', 'w+') #store in file
+            heading = f'Data For Node Id: {i.nid}\n'
+            file.write(heading)
+            for _, block in i.blockChain.items(): #each block
+                parent = None
+                miner = None
+                if block.pb != 0: #if parent and miner exists
+                    parent = block.pb.bid
+                    miner = block.miner.nid
+                log_to_write = f"Block Id:{block.bid}, Parent ID:{parent}, Miner ID:{miner}, Txns:{len(block.txnIncluded)}, Time:{block.time}\n"
+                file.write(log_to_write)
+            file.close()
+            
+    #just a switch case to handle events
+    def handle(self, event): #event handler 
+        if event.event_type == "TxnGen":
+            event.txn.peerX.txnSend(event)
+        elif event.event_type == "TxnRecv":
+            event.receiver.txnRecv(event)
+        elif event.event_type == "BlockRecv":
+            event.receiver.verifyAndAddReceivedBlock(event)
+        elif event.event_type == "BlockMined":
+            event.block.miner.receiveSelfMinedBlock(event)
+
+    #plot
+    def draw_bc(self, nid, save=False):
+        plt.figure()
+        # draw network with Kamada Kawai layout
+        nx.draw(self.nodes[nid].g, pos=nx.kamada_kawai_layout(self.nodes[nid].g), node_size=10, node_color='red')
+        if save:
+            plt.savefig(f'./figures/blockchain_{nid}.png')
+        else:
+            plt.show()
+
+    #save plot
+    def print_graph(self, save=False): #print graph     
+        plt.figure()
+        nx.draw(self.G, with_labels=True)
+        if save:
+            plt.savefig('./figures/network_graph.png')
+        else:
+            plt.show()
+
+    def print_stats(self):
+        nd = self.nodes[0]
+        genesis = 1
+
+        g_rev = nd.g.reverse()
+        succ = nx.dfs_successors(g_rev, source=genesis)
+        depth_from_root = {}
+        max_depth = {}
+        parent = {}
+        deepest_node = genesis
+
+        def dfs(u, par = None, dep = 0):
+            nonlocal g_rev, succ, depth_from_root, max_depth, parent, deepest_node
+            depth_from_root[u] = dep
+            max_depth[u] = dep
+            parent[u] = par
+            if dep > depth_from_root[deepest_node]:
+                deepest_node = u
+            if u not in succ:
+                return
+            for v in succ[u]:
+                dfs(v, u, dep + 1)
+                max_depth[u] = max(max_depth[u], max_depth[v])
+        dfs(genesis)
+
+        branches = []
+        while deepest_node != genesis:
+            child = deepest_node
+            deepest_node = parent[deepest_node]
+            for u in succ[deepest_node]:
+                if u != child:
+                    branches.append(max_depth[u] - depth_from_root[deepest_node])
+
+        node_type_successful = {}
+        node_type_blocks_mined = {}
+        for type in ['slow_low', 'slow_high', 'fast_low', 'fast_high']:
+            node_type_successful[type] = 0
+            node_type_blocks_mined[type] = 0
+        mined_in_longest_chain = {}
+        for node in self.nodes:
+            mined_in_longest_chain[node.nid] = 0
+            node_type_blocks_mined[node.speed + '_' + node.cpu] += node.created_blocks_own
+        block = nd.blockChain[nd.lbid]
+        while block.bid != genesis:
+            mined_in_longest_chain[block.miner.nid] += 1
+            node_type_successful[block.miner.speed + '_' + block.miner.cpu] += 1
+            block = block.pb
+
+        print("Length of longest chain (including genesis block):", nd.blockChain[nd.lbid].length)
+        print("Total number of blocks mined:", get_blocks())
+        print("Fraction of mined blocks present in longest chain:", round((nd.blockChain[nd.lbid].length-1) / get_blocks(), 3))
+        print()
+        for type in ['slow_low', 'slow_high', 'fast_low', 'fast_high']:
+            print(f"% blocks in longest chain mined by {type} node:", round(node_type_successful[type] / (nd.blockChain[nd.lbid].length-1), 2))
+        print()
+        for type in ['slow_low', 'slow_high', 'fast_low', 'fast_high']:
+            if node_type_blocks_mined[type] == 0:
+                print(f"% blocks mined by {type} node that made it to longest chain: 0.0")
+            else:
+                print(f"% blocks mined by {type} node that made it to longest chain:", round(node_type_successful[type] / node_type_blocks_mined[type], 2))
+        print()
+        if len(branches) > 0:
+            print("Lengths of branches:", branches)
+            print("Average length of branch:", round(np.average(branches), 3))
+        else:
+            print("No branches were formed!")
+
+
+if __name__ == "__main__":
+    #parse args
+    parser = argparse.ArgumentParser(description='a P2P network blockchain simulator')
+    parser.add_argument('-n', '--num_nodes', default=10, type=int, help='number of nodes in the P2P network')
+    parser.add_argument('-z0', '--percentage_slow', default=0.5, type=float, help='percentage of slow nodes')
+    parser.add_argument('-z1', '--percentage_lowcpu', default=0.5, type=float, help='percentage of nodes having low CPU power')
+    parser.add_argument('-ttx', '--mean_inter_arrival', default=10, type=float, help='mean inter-arrival time between transactions')
+    parser.add_argument('-I', '--average_block_mining_time', default=600, type=float, help='average time taken to mine a block')
+    parser.add_argument('-T', '--simulation_time', default=10000, type=float, help='total time for which the P2P network is simulated')
+    parser.add_argument('-s', '--save_figures', default=False, action='store_true', help='use this flag to save all figures generated in ./figures')
+
+    args = parser.parse_args()
+
+    num_nodes = args.num_nodes
+    percentage_slow = args.percentage_slow
+    percentage_lowcpu = args.percentage_lowcpu
+    mean_inter_arrival = args.mean_inter_arrival
+    average_block_mining_time = args.average_block_mining_time
+    simulation_time = args.simulation_time
+    save_figures = args.save_figures
+
+    if os.path.exists('./logs'):
+        shutil.rmtree('./logs')
+    os.mkdir('./logs')
+
+    if save_figures:
+        if os.path.exists('./figures'):
+            shutil.rmtree('./figures')
+        os.mkdir('./figures')
+
+    simulator = Simulation(num_nodes, mean_inter_arrival, percentage_slow,
+                           percentage_lowcpu, average_block_mining_time)
+    
+    simulator.generate_network()
+    simulator.print_graph(save=save_figures)
+    simulator.gen_all_txn(simulation_time)
+    simulator.run(simulation_time)
+    simulator.print_stats()
+
+    # draw blockchain
+    for i in range(num_nodes):
+        simulator.draw_bc(i, save=save_figures)
---
+++ selfish/utils/definitions.py
@@ -0,0 +1,231 @@
+import copy
+import networkx as nx
+
+from utils.seed import rng
+from utils import verifyBlock, computeLatency, pushq, incr_blocks
+
+
+class Event:
+    """
+    Event class: Stores all types of Events
+    
+    EVENT_TYPES: "TxnGen", "TxnRecv", "BlockRecv", "BlockMined"
+    """
+
+    def __init__(self, time, event_type, txn=None, block=None, sender=None, receiver=None):
+        self.time = time
+        self.event_type = event_type
+        self.txn = txn
+        self.block = block
+        self.sender = sender
+        self.receiver = receiver
+    
+
+class Block:
+    def __init__(self, bid, pb, txnIncluded, miner, balance=[]):
+        self.bid = bid # block id
+        self.pb = pb #parent block
+        self.time = 0
+        self.size = 1 + len(txnIncluded) #size of block
+        if pb != 0: #to check if it is not genesis block
+            # self.txnIncluded = copy.deepcopy(txnIncluded)
+            # txnPool stores all the txn mined till now 
+            # length shows the length of chain from genesis block till current block
+            self.txnIncluded = txnIncluded
+            self.txnPool = pb.txnPool
+            self.length = pb.length+1
+            self.balance = copy.deepcopy(pb.balance)
+        else:
+            self.txnIncluded = set()
+            self.txnPool = set()
+            self.length = 1
+            self.balance = balance
+        self.miner = miner
+        
+        for i in txnIncluded: #updating balance of all the user 
+            if i.case == 1:
+                self.balance[i.peerX.nid] -= i.value
+            self.txnPool.add(i)
+            self.balance[i.peerY.nid] += i.value
+
+class Node:
+    def __init__(self, nid, speed, cpu, ntype, genesis, miningTime, blkgen, txngen):
+        self.peers = set() # neighbours of the node
+        self.blockChain = dict() # blockchain of the node
+        self.blockReceived = set() # blocks received till now
+        self.txnReceived = set() # txn received till now 
+        self.g = nx.DiGraph() # graph
+        self.nid = nid # unique id of the node 
+        self.speed = speed # 1=fast, 0=slow
+        self.ntype = ntype #node type honest or adversary
+        self.cpu = cpu
+        self.lbid = genesis.bid
+        if ntype == "adversary":
+            self.revealed_bid = genesis.bid
+            self.child_ids = {}
+        # self.blockChain[genesis.bid] = copy.deepcopy(genesis)
+        self.blockChain[genesis.bid] = genesis
+        self.blkid_generator = blkgen
+        self.miningTime = miningTime # avg interarrival time/hashpower
+        self.txnid_generator = txngen
+        self.created_blocks_own = 0
+
+    # generates transactions
+    def txnSend(self, event):
+        if self.blockChain[self.lbid].balance[self.nid] < 1e-8: 
+            #if balance is less than 1e-8 then no txn is generated
+            return
+        event.txn.value = rng.uniform(0, self.blockChain[self.lbid].balance[self.nid])
+        if self.blockChain[self.lbid].balance[self.nid] > event.txn.value:
+            self.txnReceived.add(event.txn) #add  recieved into set
+            for i in self.peers:
+                t = event.time + computeLatency(peerX=self, peerY=i, m=1)
+                pushq(Event(t, event_type="TxnRecv", sender=self, receiver=i, txn=event.txn))
+            
+
+    # forwards transactions
+    def txnRecv(self,event):
+        if event.txn not in self.txnReceived:
+            self.txnReceived.add(event.txn)
+            for i in self.peers:
+                t = event.time + computeLatency(peerX=self, peerY=i, m=1)
+                pushq(Event(t, event_type="TxnRecv", sender=self.nid, receiver=i, txn=event.txn))
+        
+
+    # new block generation
+    def mineNewBlock(self, block, lat):
+        while True:
+            remaingTxn = self.txnReceived.difference(block.txnPool)
+            toBeDeleted = set([i for i in remaingTxn if i.value > block.balance[i.peerX.nid]])
+
+            remaingTxn = remaingTxn.difference(toBeDeleted)
+            numTxn = len(remaingTxn)
+            
+            if numTxn > 1:
+                numTxn = min(rng.integers(1, numTxn), 1023) # 1 for coinbase txn, 1 for itself
+
+            txnToInclude = set(rng.choice(list(remaingTxn), numTxn))
+            txnId = next(self.txnid_generator)
+            coinBaseTxn = Transaction(id=txnId, peerX=-1, peerY=self, value=50, case=2)
+            txnToInclude.add(coinBaseTxn)
+
+            newBlockId = next(self.blkid_generator)
+            # print(newBlockId)
+            newBlock = Block(bid=newBlockId, pb=block,
+                            txnIncluded=txnToInclude, miner=self)
+            
+            if verifyBlock(newBlock):
+                break
+
+        lat = lat + rng.exponential(self.miningTime) #takes mean not lambda
+        pushq(Event(lat, event_type="BlockMined", block=newBlock))
+
+
+    #this function is called, if block receives a node from its peers
+    #block is verified and if the block is without any errors then its is added to blockchain 
+    # and then transmitted to neighbours 
+    # If addition of that block creates a primary chain then mining is started over that block
+    def verifyAndAddReceivedBlock(self, event):
+        if self.ntype == "honest":
+            if event.block.bid not in self.blockReceived:
+                self.blockReceived.add(event.block.bid)
+                if not verifyBlock(event.block):
+                    return
+
+                event.block.time = event.time
+                self.blockChain[event.block.bid] = event.block
+                self.g.add_edge(event.block.bid, event.block.pb.bid)
+                if event.block.length > self.blockChain[self.lbid].length:
+                    self.lbid = event.block.bid
+                    self.mineNewBlock(block=event.block, lat=event.time)
+
+                for i in self.peers:
+                    lat = event.time + computeLatency(peerX=self, peerY=i, m=event.block.size)
+                    pushq(Event(lat, event_type="BlockRecv", sender=self, receiver=i, block=event.block))
+        else:
+            if event.block.bid not in self.blockReceived:
+                self.blockReceived.add(event.block.bid)
+                if not verifyBlock(event.block):
+                    return
+
+                event.block.time = event.time
+                self.blockChain[event.block.bid] = event.block
+                self.g.add_edge(event.block.bid, event.block.pb.bid)
+                if event.block.length > self.blockChain[self.lbid].length:
+                    self.lbid = event.block.bid
+                    self.revealed_bid = event.block.bid
+                    self.mineNewBlock(block=event.block, lat=event.time)
+                elif event.block.length == self.blockChain[self.lbid].length - 1:
+                    print("Case 2")
+                    print(self.child_ids)
+                    print(self.blockChain[self.revealed_bid].length)
+                    print(self.blockChain[self.lbid].length)
+                    print(event.block.length)
+                    while self.revealed_bid != self.lbid:
+                        self.revealed_bid = self.child_ids[self.revealed_bid]
+                        for i in self.peers:
+                            lat = event.time + computeLatency(peerX=self, peerY=i, m=self.blockChain[self.revealed_bid].size)
+                            pushq(Event(lat, event_type="BlockRecv", sender=self, receiver=i, block=self.blockChain[self.revealed_bid]))
+                else:
+                    print("Case 3")
+                    print(self.child_ids)
+                    print(self.blockChain[self.revealed_bid].length)
+                    print(self.blockChain[self.lbid].length)
+                    print(event.block.length)
+                    while self.blockChain[self.revealed_bid].length < event.block.length:
+                        self.revealed_bid = self.child_ids[self.revealed_bid]
+                        for i in self.peers:
+                            lat = event.time + computeLatency(peerX=self, peerY=i, m=self.blockChain[self.revealed_bid].size)
+                            pushq(Event(lat, event_type="BlockRecv", sender=self, receiver=i, block=self.blockChain[self.revealed_bid]))
+        
+    # thsi function is called once the mining of a block is completed, 
+    # If after mining the addition of block creates a primary chain then
+    # the block is shared with neighbours and mining is continued otherwise 
+    # node waits a block whose addition will, create primary chain
+    def receiveSelfMinedBlock(self, event):
+        if self.ntype == "honest":
+            if event.block.length <= self.blockChain[self.lbid].length:
+                return
+
+            event.block.time = event.time
+            incr_blocks()
+            self.created_blocks_own += 1
+
+            self.blockChain[event.block.bid] = event.block
+            self.g.add_edge(event.block.bid, event.block.pb.bid)
+            self.blockReceived.add(event.block.bid)
+            self.lbid = event.block.bid
+            for i in self.peers:
+                lat = event.time + computeLatency(peerX=self, peerY=i, m=event.block.size)
+                pushq(Event(lat, event_type="BlockRecv", sender=self, receiver=i, block=event.block))
+            self.mineNewBlock(block=event.block, lat=event.time)
+        else:
+            if event.block.length <= self.blockChain[self.lbid].length:
+                return
+
+            event.block.time = event.time
+            # incr_blocks()
+            self.created_blocks_own += 1
+
+            self.blockChain[event.block.bid] = event.block
+            self.g.add_edge(event.block.bid, event.block.pb.bid)
+            self.blockReceived.add(event.block.bid)
+            self.lbid = event.block.bid
+            self.child_ids[event.block.pb.bid] = event.block.bid
+
+
+class Transaction:
+    #TxnID: IDx pays IDy C coins
+    def __init__(self, id, peerX, peerY, value, case) -> None:
+        self.txnId = id
+        self.peerX = peerX
+        self.peerY = peerY
+        self.value = value
+        self.case = case
+        self.size = 1
+
+    def __repr__(self) -> str:
+        if(self.case==1):
+            return str(self.txnId)+": "+str(self.peerX)+" pays "+str(self.peerY)+" "+str(self.coins)+" coins"
+        if(self.case==2):
+           return str(self.txnId)+": "+str(self.peerX)+" mines "+str(self.coins)+" coins"
